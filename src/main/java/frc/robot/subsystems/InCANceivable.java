565a1ad225a1d2adcceadec46ba3f678684684c0
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-01-29 13:05:48 -0600

    Multiplexer and reading from CAN arduino.

--- src/main/java/frc/robot/subsystems/InCANceivable.java
+++ src/main/java/frc/robot/subsystems/InCANceivable.java
@@ -5,19 +5,29 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.hal.CANAPIJNI;
 import edu.wpi.first.hal.CANData;
+import edu.wpi.first.hal.I2CJNI;
 import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class InCANceivable extends SubsystemBase {
-    private int dvc_num = 5;
+    private int dvc_num;
+
+    // MuxColorSensorV3 cs = new MuxColorSensorV3(Port.kOnboard);
+    ColorSensorV3 cs = new ColorSensorV3(Port.kOnboard);
+    I2C multiplexer = new I2C(Port.kOnboard, 0x70);
 
     private static final int MFG = 8; // Fill in from InCANCievable github
     private static final int TYPE = 10; // Fill in from InCANCievable github
 
     CAN conn;
-    CANData recv;
+    CANData recv = new CANData();
 
     // Note: API ID (class) is 6 bits
 
@@ -29,10 +39,29 @@ public class InCANceivable extends SubsystemBase {
     @Override
     public void periodic() {
         // ...Periodically called whenever WPILib feels like it :)
-        if (conn.readPacketLatest(25, recv)) {
+        // System.out.println(recv.length);
+        if (conn.readPacketNew(25, recv)) {
             // Received data
-            System.out.println(recv.data[0]);
+            String s = "";
+            for (int i = 0; i < recv.length; i++)
+                s += ", " + Byte.toString(recv.data[i]);
+            s = s.substring(2);
+            System.out.printf("Received %d bytes: [%s]\n", recv.length, s);
         }
+
+        multiplexer.write(0, 1);
+
+        Integer red = (int) (cs.getColor().red * 255 * 1.6);
+        Integer green = (int) (cs.getColor().green * 255 * 1.4);
+        Integer blue = (int) (cs.getColor().blue * 255 * 1.6);
+
+        System.out.printf("(%d, %d, %d)\n", red, green, blue);
+
+        conn.writePacket(new byte[] {
+                red.byteValue(),
+                green.byteValue(),
+                blue.byteValue()
+        }, 25);
     }
 
     // @Override
